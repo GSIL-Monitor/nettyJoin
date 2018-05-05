544531662f1b12c86168cdabd73ff832b190cefa
wb-xxb255887 <wb-xxb255887@alibaba-inc.com>
2018-05-06 00:51:56 +0800

    并发容器学习 1.学习future的使用 2.学习blockingqueue 3.学习ThreadExecutorPool使用

---
+++ commonTest/src/test/java/com/thread/future/FutureTest.java
@@ -0,0 +1,64 @@
+package com.thread.future;
+
+import sun.awt.windows.ThemeReader;
+
+import java.util.concurrent.Callable;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.Future;
+import java.util.concurrent.FutureTask;
+
+/**
+ * 异步命令框架 future练习
+ *
+ * 假设该场景：
+ *  我现在即想挂机打怪，又想写作业
+ *
+ * @author wb-xxb255887
+ * @version $$Id: bkpartner-parent, v 0.1 2018/05/05 22:18 wb-xxb255887 Exp $$
+ */
+public class FutureTest {
+
+    private static int count = 0;
+
+    public static void main(String[] args) throws ExecutionException, InterruptedException {
+        Callable<String> gameCallable = new Game();
+        FutureTask gameFutureTask = new FutureTask<String>(gameCallable);
+//        System.out.println(gameFutureTask.isCancelled()); //false
+//        System.out.println(gameFutureTask.isDone());  //false
+        //开始挂机了，与此同时我要去写作业了
+        new Thread(gameFutureTask).start();
+
+        //写作业可以是主线程跑，也可以单起一个线程跑，最后都是判断游戏是否打完，gameFutureTask.isDone()
+        new Thread(new Homework()).start();
+
+        if (gameFutureTask.isDone()) {
+            System.out.println("task is done.");
+            System.out.println(gameFutureTask.get());
+        }
+    }
+
+    static class Game implements Callable<String>{
+
+        @Override
+        public String call() throws Exception {
+//            if (count == 0) {
+//                throw new IllegalArgumentException("参数有误");
+//            }
+            Thread.sleep(3000L);
+            return "挂机打游戏无所不能！";
+        }
+    }
+
+    static class Homework implements Runnable{
+
+        @Override
+        public void run() {
+            try {
+                Thread.sleep(3000L);
+                System.out.println("写完作业了");
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+}
